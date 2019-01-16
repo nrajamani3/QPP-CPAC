af2d0ee7bd883e0e3e694e29ee92860bf411ed39
nrajamani3 <nrajamani3@gatech.edu>
2019-01-16 12:03:11 -0500

    added new function call for regress qpp

--- detectqppv.py
+++ detectqppv.py
@@ -1,3 +1,4 @@
+
 import h5py
 import numpy as np
 import os
@@ -6,11 +7,13 @@ from scipy import stats
 from scipy import io
 from QPPv0418 import qppv
 
+
 def merger(d2,d3,d4,d5,nsubj,nrn):
 
     d2_file = h5py.File(d2)
     d2 = d2_file['B_2']
     d2 = np.array(d2)
+
     D2 = d2.reshape(d2.shape[0]*d2.shape[1],d2.shape[2])
 
     d3_file = h5py.File(d3)
@@ -33,9 +36,13 @@ def merger(d2,d3,d4,d5,nsubj,nrn):
     D_list = [D2,D3,D4,D5]
 
 
+
     return D,D_list
 
-def detect(D,D_list,msk,wl,nrp,cth,n_itr_th,mx_itr,pfs,nsubj,nrn):
+
+def detect(img_affine,D,D_list,msk,wl,nrp,cth,n_itr_th,mx_itr,pfs,nsubj,nrn):
+
+
     if  msk.endswith('.nii'):
         #data1 = nib.load(args.d)
         #data1 = np.array(data1.dataobj) #(61,73,61,8)
@@ -56,7 +63,7 @@ def detect(D,D_list,msk,wl,nrp,cth,n_itr_th,mx_itr,pfs,nsubj,nrn):
         m_voxels = np.prod(msk_img.shape[:-1])
         msk = msk_img.reshape(m_voxels,msk_img.shape[-1])
 
-    print(D.shape)
+
     nx = D.shape[3]
     nt = D.shape[2]
     nsubj = D.shape[0]
@@ -86,7 +93,15 @@ def detect(D,D_list,msk,wl,nrp,cth,n_itr_th,mx_itr,pfs,nsubj,nrn):
     #n_itr_th =1
     #mx_itr=15
     #pf2 =
-    f = qppv(B, msk, nd, wl, nrp, cth, n_itr_th, mx_itr, pfs)
+
+    #generate qpp
+    time_course, ftp, itp, iter = qppv(img_affine,B, msk, nd, wl, nrp, cth, n_itr_th, mx_itr, pfs)
+    #choose best template
+    FTP1,C_1,Met1 = BSTT(time_course,ftp)
+    #regress QPP
+    T =TBLD2WL(B,wl,FTP1)
+    Br, C1r=regressqpp(B, nd, T, C_1)
+
 
 
 if __name__ == "__main__":
@@ -113,8 +128,10 @@ if __name__ == "__main__":
     parser.add_argument("nrn",type=int,help='provide the number of runs per subject')
     args = parser.parse_args()
 
-
-
+    img_005004 = nib.load('/home/nrajamani/Downloads/0050004_bet.nii')
+    img_005004_array = np.array(img_005004.dataobj)
+    img_affine = img_005004.affine
+    print(img_005004_array[50:60])
 
     d2 = os.path.join('/home/nrajamani/Downloads/', '0050004.mat')
     d3 = os.path.join('/home/nrajamani/Downloads/', '0050005.mat')
@@ -122,4 +139,8 @@ if __name__ == "__main__":
     d5 = os.path.join('/home/nrajamani/Downloads/', '0050007.mat')
     D,D_list = merger(d2, d3, d4, d5,args.nsubj,args.nrn)
 
-    detect(D,D_list,args.msk,args.wl,args.nrp,args.cth,args.n_itr_th,args.mx_itr,args.pfs,args.nsubj,args.nrn)
+
+    detect(img_affine,D,D_list,args.msk,args.wl,args.nrp,args.cth,args.n_itr_th,args.mx_itr,args.pfs,args.nsubj,args.nrn)
+
+
+
