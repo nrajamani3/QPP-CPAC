bc43fa9e27deeff8cb94bb0ec027bba089eb9c66
nrajamani3 <nrajamani3@gatech.edu>
2019-01-22 15:26:38 -0500

    fix BSTT func args

--- detectqppv.py
+++ detectqppv.py
@@ -98,7 +98,7 @@ def detect(img,msk,wl,nrp,cth,n_itr_th,mx_itr,pfs,nsubj,nrn):
     #generate qpp
     time_course, ftp, itp, iter = qppv(B, msk, nd, wl, nrp, cth, n_itr_th, mx_itr, pfs)
     #choose best template
-    C_1,FTP1,Met1 = BSTT(time_course,ftp)
+    C_1,FTP1,Met1 = BSTT(time_course,ftp,nd,nt)
     #regress QPP
 
     T =TBLD2WL(B,wl,FTP1)
